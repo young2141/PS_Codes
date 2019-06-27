371f1ba30a6c7e326224d6d717898d7be4d5d136
이태영 <taeyounglee93@gmail.com>
2019-06-27 22:15:26 +0900

    lv4. wrong answer

--- unsolved/sw2819.py
+++ unsolved/sw2819.py
@@ -1,30 +1,29 @@
-#Runtime error (maximum recursion)
+#wrong answer... for some reason... 
 test_size = int(input())
-dy = [-1,0,1,0]
-dx = [0,1,0,-1]
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
+visit = set()
 
-def check(s,y,x):
+
+def check(s, y, x, m):
 	if len(s) == 7:
-		if visit[int(s)] == 0 :
-			visit[int(s)] = 1
-			return 1
-		else:
-			return 0
-	else :
-		for i in range(4):
-			newy = y + dy[i]
-			newx = x + dx[i]
-			if(newy <0 or newy >= 4 or newx <0 or newx >= 4): continue
-			s += str(w[newy][newx])
-			check(s,newy,newx)
+		visit.add(s)
+		return
+	for ii in range(4):
+		newy = y + dy[ii]
+		newx = x + dx[ii]
+		if newy < 0 or newy >= 4 or newx < 0 or newx >= 4: continue
+		check(s+str(m[newy][newx]), newy, newx, m)
+
 
 for test_num in range(1, test_size+1):
-	visit = [0] * 10000000
 	w = [[int(x) for x in input().split()] for y in range(4)]
 	answer = 0
 	for i in range(4):
 		for j in range(4):
-			answer += check("",i,j)
+			check("", i, j, w)
+	answer = len(visit)
+	#print(visit)
 	print("#%d %d"%(test_num, answer))
 
 '''
