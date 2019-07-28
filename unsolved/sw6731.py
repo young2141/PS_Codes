1e6e601fc8469176ffeceb74f8f585de10ab8745
이태영 <taeyounglee93@gmail.com>
2019-07-28 12:48:50 +0900

    lv4. no way to grade

--- unsolved/sw6731.py
+++ unsolved/sw6731.py
@@ -1,60 +1,50 @@
-#sw expert academy 6731 lv4 bfs(brute-force)
+# sw expert academy 6731 lv4 
+# no way to grade it. looked up 
 
-dy = [-1,0,1,0]
-dx = [0,1,0,-1]
+dy = [-1, 0, 1, 0]
+dx = [0, 1, 0, -1]
 
-def printGraph(w,n):
+
+def printGraph(w, n):
+    print()
+    for i in range(n):
+        for j in range(n):
+            print(w[i][j], end='')
         print()
-        for i in range(n):
-                for j in range(n): 
-                        print(w[i][j], end ='')
-                print()
 
-def coloring(w,n,i,j):
-        for x in range(n):
-                w[i][x] = w[i][x] * -1
-        for y in range(n):
-                w[y][j] = w[y][j]* -1
-        w[i][j] = w[i][j] * -1
-def uncolor(w,n,i,j):
-        for x in range(n):
-                w[i][x] = w[i][x] * -1
-        for y in range(n):
-                w[y][j] = w[y][j]* -1
-        w[i][j] = w[i][j] * -1
-                        
+def search(w,n):
+	ret = 0
+	g = [[0] * n for i in range(n)]
+	for i in range(n):
+		for j in range(n):
+			addUp = 0 - w[i][j]
+			for y in range(n):
+				addUp += w[y][j]
+			for x in range(n):
+				addUp += w[i][x]
+			g[i][j] = addUp
+			if addUp % 2 == 1:
+				ret += 1
 
-def search(w,g,n,c):
-        if c >= n*n :
-                return 0
-
-        for i in range(n):
-                for j in range(n):
-                        coloring(g,n,i,j)
-                        if w==g : return c
-                        c = search(w,g,n,c+1)
-                        uncolor(g,n,i,j)
-        return 0             
+	#printGraph(g,n)
+	return ret
 
 tc = int(input())
-for tc in range(1,tc+1):
+for tc in range(1, tc + 1):
     n = int(input())
-    w = [[1]*n for i in range(n)]
-    g = [[1]*n for i in range(n)]
+    w = [[0] * n for i in range(n)]
 
     for i in range(n):
         s = list(input())
         for j, color in enumerate(s):
-                if color =='W':
-                        w[i][j] = 1
-                else :
-                        w[i][j] = -1
-    printGraph(w,n)
-    if w != g :
-        answer = search(w,g,n,0)
-    else: answer = 0
-    print('#%d %d' % (tc, answer))
+            if color == 'W':
+                w[i][j] = 0
+            else:
+                w[i][j] = 1
+    #printGraph(w, n)
+    answer = search(w,n)
 
+    print('#%d %d' % (tc, answer))
 
 '''
 4
