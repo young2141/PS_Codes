7a6224a7fff36cdc3d1623182e422e0c36ed6cba
young-desktop <taeyounglee93@gmail.com>
2019-10-14 23:47:31 +0900

    ...

--- swea4311.py
+++ swea4311.py
@@ -1,17 +1,12 @@
-import sys
-import copy
-from collections import deque
-sys.stdin = open('input.txt','r')
-
 def calc(a,b,op):
     a,b = int(a), int(b)
-    if op == 1: 
-        if op <= 999 : return a+b 
-    elif op == 2: 
-        if op >= 0 : return a-b 
-    elif op == 3: 
-        if op <= 999 : return a*b
-    elif op == 4: 
+    if op == '1': 
+        if a+b <= 999 : return a+b 
+    elif op == '2': 
+        if a-b >= 0 : return a-b 
+    elif op == '3': 
+        if a*b <= 999 : return a*b
+    elif op == '4': 
         if b : return a//b    
     return -1
 
@@ -21,43 +16,48 @@ for tc in range(1, tc+1):
     numbers = input().split()
     ops = input().split()
     goal = input()
-    answer = sys.maxsize
+    answer = 987654321
     tester = list(goal)
     for t in tester:
         if t not in numbers: break
     else: answer = len(goal)
     
-    visit = [0 for _ in range(1000)]
-    
-    q = deque([['', 0]])
+    visit = [0 for _ in range(1000)]    
+    q = [['', 0]]
     
     while q:
-        print(q)
-        s,count = q.popleft()
-        if count > max_hit : continue
+        s,cnt = q[0]
+        q = q[1:]
+        if cnt > max_hit : continue
         
         if s == goal:
-            answer = min(answer, count)
+            answer = min(answer, cnt+1)
             continue
-            
-        candi = [[s,count]]
+
+        candi = [[s,cnt]]
         if len(s) <3 :
             for num in numbers:
-                candi.append([s+num,count+1])
+                candi.append([s+num,cnt+1])
         for cn,cc in candi:
             if cn == '': continue
             cn = int(cn)
-            for num in numbers:
-                num = int(num)            
+            
+            num = ''
+            for a in numbers:
+                for b in numbers:
+                    for c in numbers:
+                        
+
+                num = int(num)
                 for op in ops:
                     result =  calc(cn,num,op)
                     if result != -1:
-                        if visit[result] == 0 or visit[result] > cc +1:
-                            visit[result] = cc+1
-                            q.append([str(result), cc+1])
+                        if visit[result] == 0 or visit[result] > cc +2:
+                            visit[result] = cc+2
+                            q.append([str(result), cc+2])
                             
-    if answer == sys.maxsize: answer = -1
-    print(answer)
+    if answer == 987654321: answer = -1
+    print('#%d %d'%(tc, answer))
                 
             
         
