558084ae33f83cf58bf6c1f941d3fbc1928fddd4
taeyoung <taeyounglee93@gmail.com>
2019-12-11 00:39:47 +0900

    12.11

--- moveArea.java
+++ moveArea.java
@@ -1,19 +1,50 @@
-public class moveArea {
-    public static void main(String[] args) {
-        int[][] land = { { 1, 4, 8, 10 }, { 5, 5, 5, 5 }, { 10, 10, 10, 10 }, { 10, 10, 10, 20 } };
-        int height = 3;
+package sol;
 
-        int[][] visit = new int[land.length][land[0].length];
-        for (int[] v_line : visit) {
-            for (int v : v_line) {
-                System.out.print(v);
-            }
-            System.out.println();
-        }
-        return;
+import java.awt.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution {
+    public int[] dy = {-1,0,1,0};
+    public int[] dx = {0,1,0,-1};
+    public boolean[][] visit;
+
+    public int solution(int[][] land, int height) {
+        int answer = 0;
+
+        int h = land.length, w = land[0].length,area = 0;
+        visit = new boolean[h][w];
+
+        for(int i=0;i<h;++i)
+            for(int j=0;j<w;++j)
+                if(!visit[i][j]){
+                    area++;
+                    dfs(land,i,j,height);
+                }
+
+        return area;
     }
 
-    public static void dfs(int[][] land, int[][] visit) {
+    public void dfs(int[][] land, int i, int j,int height){
+        int h = land.length, w = land[0].length;
+        Queue<Point> q = new LinkedList<Point>();
+        q.add(new Point(i,j));
+        visit[i][j] = true;
 
+        while(!q.isEmpty()){
+            int y = q.peek().y;
+            int x = q.peek().x;
+            q.remove();
+
+            for(int k=0;k<4;++k){
+                int ny = y + dy[k];
+                int nx = x + dx[k];
+                if(ny < 0 || ny >= h || nx < 0 || nx >= w) continue;
+                if(!visit[ny][nx] && Math.abs(land[ny][nx] - land[i][j])<= height ) {
+                    visit[ny][nx] = true;
+                    dfs(land, ny,nx, height);
+                }
+            }
+        }
     }
 }
